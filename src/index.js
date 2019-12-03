a0ee5afc781e43cd1e2b5884208033965ca98cbd
Ido Ayal <idodidodi@gmail.com>
2019-12-03 17:32:14 +0200

    With initial value

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@
 // TODO - move winows checks and calcs to new func
 // call this function on window change
 // fix resizing bug
+// release drag if mouse going out of path X 20 px outside path y for ech side
 
-function sliderCmp(minRange, maxRange, currentPos) {
+sliderCmp(30, 70, 58)
+
+function sliderCmp(minRange, maxRange, currentValue) {
+    // debugger
     const range = maxRange - minRange
     var windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    let isDragged = false;
     const pivotLeftFactor = 58;
     const pivotEl = document.getElementById('pivot')
     const pathEl = document.getElementById('slider-path')
     const pathWidthStr = getComputedStyle(pathEl).width
     const pathWidth = pathWidthStr.slice(0, pathWidthStr.indexOf('px'))
     const margin = windowWidth - pathWidth
-    setCurrentValue(currentPos)
+    setNewPosToPivot(null, currentValue)
     registerEventListeners()
 
     function registerEventListeners() {
@@ -33,7 +36,7 @@ function sliderCmp(minRange, maxRange, currentPos) {
                     registerToDragSlider(pivotEl);
                     return
                 default:
-                    if (!isDragged) { UnRegisterToDragSlider() }
+                    UnRegisterToDragSlider()
                     setNewPosToPivot(ev)
             }
         }
@@ -42,11 +45,15 @@ function sliderCmp(minRange, maxRange, currentPos) {
     function isInsideSliderPath(xPos) {
         return xPos > margin / 2 && xPos < (windowWidth - margin / 2) ? true : false
     }
-    function setNewPosToPivot(ev) {
-        const xPos = ev.clientX
+    function setNewPosToPivot(ev, currentValue) {
+        const xPos = ev
+            ? ev.clientX
+            : ((currentValue - minRange) / range * pathWidth) + (margin / 2)
         if (isInsideSliderPath(xPos)) {
             pivotEl.style.left = xPos - pivotLeftFactor
-            const value = (xPos - (margin / 2)) * range / pathWidth
+            const value = ev
+                ? (xPos - (margin / 2)) * range / pathWidth
+                : currentValue - minRange
             setCurrentValue(value)
         }
     }
@@ -60,8 +67,6 @@ function sliderCmp(minRange, maxRange, currentPos) {
     }
 
     function setCurrentValue(value) {
-        document.getElementById('current-value').innerText = Math.round(value)
+        document.getElementById('current-value').innerText = Math.round(value) + minRange
     }
 }
-
-sliderCmp(0, 100, 50)
