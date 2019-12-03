a8199a3e9e641197762553fa142cda31e1eadc15
Ido Ayal <idodidodi@gmail.com>
2019-12-03 15:44:08 +0200

    Dragging works

--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
 'use strict'
 
-function init() {
-    listenToMouseEvents()
+function sliderCmp(minRange, maxRange, currentPos) {
+    let isDragged = false;
+    const leftMargin = 58;
+    const pivotEl = document.getElementById('pivot')
+    setCurrentValue(currentPos)
+    registerEventListeners()
+
+    function registerEventListeners() {
+        const pathEl = document.getElementById('slider-path')
+        pathEl.addEventListener('click', moveSlider)
+        pathEl.addEventListener('mousedown', moveSlider)
+        // pathEl.addEventListener('mouseup', moveSlider)
+    }
+
+    function moveSlider(ev) {
+        console.log('should see you many times while dragging')
+        const eventType = ev.type
+        if (ev.clientX > 30) {
+
+            switch (eventType) {
+                // case 'mouseup':
+                //     console.log('mouse up', ev.offsetY)
+                // return
+                case 'mousedown':
+                    pivotEl.style.left = ev.clientX - leftMargin;
+                    registerToDragSlider(pivotEl);
+                    console.log('should see you once while dragging')
+                    return
+                default:
+                    if (!isDragged) { UnRegisterToDragSlider() }
+                    pivotEl.style.left = ev.clientX - leftMargin
+            }
+        }
+        // TODO add clientX < ....
+    }
+
+    function setNewPosToPivot(ev) {
+        const xPos = ev.clientX
+        if (xPos > 36) {
+            pivotEl.style.left = xPos - leftMargin
+        }
+    }
+
+    function registerToDragSlider() {
+        pivotEl.addEventListener('mousemove', setNewPosToPivot)
+    }
+
+    function UnRegisterToDragSlider() {
+        pivotEl.removeEventListener('mousemove', setNewPosToPivot)
+    }
+
+    function setCurrentValue(value) {
+        document.getElementById('current-value').innerText = value
+    }
 }
 
-function listenToMouseEvents() {
-    const el = document.getElementById('slider-path')
-    el.addEventListener('click', moveSlider)
-}
-
-function moveSlider(ev) {
-    const pivot = document.getElementById('pivot')
-    pivot.style.left = ev.clientX - 58
-}
+sliderCmp(0, 100, 50)
