93152f5dac38c21ab33416d4b40b96a380f59e8a
Ido Ayal <idodidodi@gmail.com>
2019-12-04 05:29:06 +0200

    Animation works

--- src/index.js
+++ src/index.js
@@ -1,28 +1,49 @@
 'use strict'
 
-// TODO - move winows checks and calcs to new func
-// call this function on window change
-// fix resizing bug
 // release drag if mouse going out of path X 20 px outside path y for ech side
+// see in the browser on 100% and enlarge
+// fix bug on release - can't release on path ends
+// can press outside path to cancel but then value would set to max range
 
 sliderCmp(30, 70, 58)
 
-function sliderCmp(minRange, maxRange, currentValue) {
-    // debugger
+function sliderCmp(minRange, maxRange, currentPos) {
     const range = maxRange - minRange
-    var windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    const pivotLeftFactor = 58;
+    const pivotLeftPos = 58;
     const pivotEl = document.getElementById('pivot')
     const pathEl = document.getElementById('slider-path')
-    const pathWidthStr = getComputedStyle(pathEl).width
-    const pathWidth = pathWidthStr.slice(0, pathWidthStr.indexOf('px'))
-    const margin = windowWidth - pathWidth
+    const circle = document.getElementById('circle')
+    let currentValue = currentPos
+
     setNewPosToPivot(null, currentValue)
     registerEventListeners()
 
     function registerEventListeners() {
         pathEl.addEventListener('click', moveSlider)
         pathEl.addEventListener('mousedown', moveSlider)
+        window.addEventListener('resize', onWindowResize)
+        circle.addEventListener('mousedown', animateCircle)
+    }
+
+    function animateCircle() {
+        circle.classList.add('shape-circle')
+    }
+
+    function onWindowResize() {
+        setNewPosToPivot(null, currentValue)
+    }
+
+    function getPathMargins() {
+        return getWindowWidth() - getPathWidth()
+    }
+
+    function getPathWidth() {
+        const pathWidthStr = getComputedStyle(pathEl).width
+        return pathWidthStr.slice(0, pathWidthStr.indexOf('px'))
+    }
+
+    function getWindowWidth() {
+        return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     }
 
     function moveSlider(ev) {
@@ -32,27 +53,28 @@ function sliderCmp(minRange, maxRange, currentValue) {
 
             switch (eventType) {
                 case 'mousedown':
-                    pivotEl.style.left = ev.clientX - pivotLeftFactor;
+                    animateCircle()
+                    pivotEl.style.left = ev.clientX - pivotLeftPos;
                     registerToDragSlider(pivotEl);
                     return
                 default:
-                    UnRegisterToDragSlider()
+                    UnRegisterFromDragSlider()
                     setNewPosToPivot(ev)
             }
         }
     }
 
     function isInsideSliderPath(xPos) {
-        return xPos > margin / 2 && xPos < (windowWidth - margin / 2) ? true : false
+        return xPos > getPathMargins() / 2 && xPos < (getWindowWidth() - getPathMargins() / 2) ? true : false
     }
     function setNewPosToPivot(ev, currentValue) {
         const xPos = ev
             ? ev.clientX
-            : ((currentValue - minRange) / range * pathWidth) + (margin / 2)
+            : ((currentValue - minRange) / range * getPathWidth()) + (getPathMargins() / 2)
         if (isInsideSliderPath(xPos)) {
-            pivotEl.style.left = xPos - pivotLeftFactor
+            pivotEl.style.left = xPos - pivotLeftPos
             const value = ev
-                ? (xPos - (margin / 2)) * range / pathWidth
+                ? (xPos - (getPathMargins() / 2)) * range / getPathWidth()
                 : currentValue - minRange
             setCurrentValue(value)
         }
@@ -62,11 +84,14 @@ function sliderCmp(minRange, maxRange, currentValue) {
         pivotEl.addEventListener('mousemove', setNewPosToPivot)
     }
 
-    function UnRegisterToDragSlider() {
+    function UnRegisterFromDragSlider() {
         pivotEl.removeEventListener('mousemove', setNewPosToPivot)
+        circle.classList.remove('shape-circle')
+
     }
 
     function setCurrentValue(value) {
-        document.getElementById('current-value').innerText = Math.round(value) + minRange
+        currentValue = Math.round(value) + minRange
+        document.getElementById('current-value').innerText = currentValue
     }
 }
