382aa9a9dfbe5c20f88ab24ef176763a4ca23535
Ido Ayal <idodidodi@gmail.com>
2019-12-04 08:25:22 +0200

    Animation bugs fixed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 'use strict'
 
-// release drag if mouse going out of path X 20 px outside path y for ech side
-// see in the browser on 100% and enlarge
-// fix bug on release - can't release on path ends
-// can press outside path to cancel but then value would set to max range
+// add simple form to change cmp's input
+// refactor and add comments
 
 sliderCmp(30, 70, 58)
 
 function sliderCmp(minRange, maxRange, currentPos) {
     const range = maxRange - minRange
-    const pivotLeftPos = 58;
+    const pivotAbsPos = 58;
     const pivotEl = document.getElementById('pivot')
     const pathEl = document.getElementById('slider-path')
     const circle = document.getElementById('circle')
+    const leftTransitionProp = pivotEl.style.transition
     let currentValue = currentPos
 
     setNewPosToPivot(null, currentValue)
@@ -27,6 +26,9 @@ function sliderCmp(minRange, maxRange, currentPos) {
 
     function animateCircle() {
         circle.classList.add('shape-circle')
+        setTimeout(() => {
+            circle.classList.remove('shape-circle')
+        }, 1000);
     }
 
     function onWindowResize() {
@@ -39,7 +41,7 @@ function sliderCmp(minRange, maxRange, currentPos) {
 
     function getPathWidth() {
         const pathWidthStr = getComputedStyle(pathEl).width
-        return pathWidthStr.slice(0, pathWidthStr.indexOf('px'))
+        return +pathWidthStr.slice(0, pathWidthStr.indexOf('px'))
     }
 
     function getWindowWidth() {
@@ -54,11 +56,10 @@ function sliderCmp(minRange, maxRange, currentPos) {
             switch (eventType) {
                 case 'mousedown':
                     animateCircle()
-                    pivotEl.style.left = ev.clientX - pivotLeftPos;
+                    pivotEl.style.left = ev.clientX - pivotAbsPos;
                     registerToDragSlider(pivotEl);
                     return
                 default:
-                    UnRegisterFromDragSlider()
                     setNewPosToPivot(ev)
             }
         }
@@ -70,18 +71,25 @@ function sliderCmp(minRange, maxRange, currentPos) {
     function setNewPosToPivot(ev, currentValue) {
         const xPos = ev
             ? ev.clientX
-            : ((currentValue - minRange) / range * getPathWidth()) + (getPathMargins() / 2)
+            : getXposFromSliderValue(currentValue)
         if (isInsideSliderPath(xPos)) {
-            pivotEl.style.left = xPos - pivotLeftPos
+            pivotEl.style.left = xPos - pivotAbsPos
             const value = ev
-                ? (xPos - (getPathMargins() / 2)) * range / getPathWidth()
+                ? getSliderValueFromXpos(xPos)
                 : currentValue - minRange
             setCurrentValue(value)
         }
     }
+    function getXposFromSliderValue(value) {
+        return ((value - minRange) / range * getPathWidth()) + (getPathMargins() / 2)
+    }
 
+    function getSliderValueFromXpos(xPos) {
+        return (xPos - (getPathMargins() / 2)) * range / getPathWidth()
+    }
     function registerToDragSlider() {
         pivotEl.addEventListener('mousemove', onDrag)
+        window.addEventListener('mouseup', UnRegisterFromDragSlider)
     }
 
     function onDrag(ev) {
@@ -90,9 +98,9 @@ function sliderCmp(minRange, maxRange, currentPos) {
     }
 
     function UnRegisterFromDragSlider() {
+        window.removeEventListener('mouseup', UnRegisterFromDragSlider)
         pivotEl.removeEventListener('mousemove', onDrag)
-        circle.classList.remove('shape-circle')
-        pivotEl.style.transition = 'left 1s'
+        pivotEl.style.transition = leftTransitionProp // Re-add CSS transition on drag
     }
 
     function setCurrentValue(value) {
